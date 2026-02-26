2a78481a89b962bc858ef52514d228b46afac30d
Md Samiul Alam Rifad <arafatefad@gmail.com>
2026-02-26 13:29:35 +0200

    Lab 0: environment setup

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Lab 0 setup successful");
+Console.WriteLine($"OS: {Environment.OSVersion}");
+Console.WriteLine($".NET: {Environment.Version}");
+
